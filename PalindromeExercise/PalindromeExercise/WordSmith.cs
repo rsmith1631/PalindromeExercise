a11d31e5275cf8a69376b7e128ea263e1ea8d23c
rsmith1631 <rsmith1631@gmail.com>
2023-08-03 00:38:32 -0700

    Palidrome Exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalidrome(string x)
+        {
+            string reverse = "";
+
+            for (int i = x.Length - 1; i >= 0; i--)
+            {
+                reverse += x[i];
+            }
+            if (x == reverse)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,28 @@
+using PalindromeExercise;
 using System;
+using System.Security.Cryptography.X509Certificates;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [InlineData("racecar", true)]
+        [InlineData("kayak", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("civic", true)]
+        [InlineData("hello", false)]
+        [InlineData("Good", false)]
+        [InlineData("", false)]
+        [InlineData("null", false)]
+        public void Test1(string x, bool expected)
         {
-
+            //Arrange
+            WordSmith  wordReversed = new WordSmith();
+            //Act
+            var result = wordReversed.IsAPalidrome(x);
+            //Assert
+            Assert.Equal(expected, result);
         }
     }
 }
